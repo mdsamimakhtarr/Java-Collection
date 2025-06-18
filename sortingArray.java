7c476761e28c62b42ecc66b0b06c4b17d67d6e09
md samim akhtar <mdsamimakhtar0000@gmail.com>
2025-06-19 00:44:50 +0530

    Sorting Integer Element

---
+++ sortingArray.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class sortingArray {
+  public static void main(String[] args) {
+    ArrayList<Integer> list=new ArrayList<>();
+    list.add(2);
+    list.add(10);
+    list.add(8);
+    list.sort(null);
+
+    System.out.println(list); // sorting an Array
+  }
+}
